eba93bc88aa395b0cadf3bf2c581795fec60272e
Shashank Singh <117472396+Shashank-Singh-05@users.noreply.github.com>
2026-03-28 23:36:13 +0530

    Update script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 5: Open Source Manifesto Generator
-# Author: BhShashank Singh
+# Author: Shashank Singh
 # Course: Open Source Software
 
 echo "Answer the following questions to generate your Open Source Manifesto."
